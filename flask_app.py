3fc2b596ed0d1f4680db965ba2a210c7bc0c2787
Anderson-Alves1994 <anderson.alves@aluno.faculdadeimpacta.com.br>
2020-03-12 00:49:00 +0000

    flask

---
+++ flask_app.py
@@ -0,0 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def inicio():
+    return render_template('index.html')
+
